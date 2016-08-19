ba7ad6e40caccec339a0b0ed6d940f86b3d99b03
Sami Kurvinen <sami.kurvinen@ray.fi>
2016-08-19 10:37:42 +0300

    Logs usernames

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ server.use(restify.bodyParser());
 var messages = {}
 
 server.get("/users/:name/messages", function(req, res, next){
-	console.log('GETTING MESSAGES')
+	console.log(`GETTING MESSAGES FOR ${req.params.name}`)
 	msgs = messages[req.params.name]
 	res.send(msgs && msgs.length > 0 ? msgs.shift() : [])
 	next()
 });
 
 server.post("/users/:name/messages", function(req, res, next){
-	console.log('SETTING MESSAGES')
+	console.log(`ADDING MESSAGES FOR ${req.params.name}`)
 	if(messages[req.params.name]) {
 		messages[req.params.name].push(req.params.message)
 	} else {
